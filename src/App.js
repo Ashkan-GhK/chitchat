e286d9e0a46f3f3a9bddce747e1dada462ab0805
Ashkan Gholizadeh <ashkan.ghk84@gmail.com>
2021-06-02 13:26:01 +0100

    router & preview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import Preview from './Preview';
+import WebcamCapture from './WebcamCapture';
 
 function App() {
   return (
-    <div className="App">
-      <h1>ChitChat</h1>
+    <div className="app">
+      <Router>
+            <div className="app__body">
+              <div className="app__bodyBackground">
+                <Switch>
+                <Route path="/preview">
+                    <Preview />
+                  </Route>
+                  <Route exact path="/">
+                    <WebcamCapture />
+                  </Route>
+                </Switch>
+              </div>
+            </div>
+      </Router>
     </div>
   );
 }
